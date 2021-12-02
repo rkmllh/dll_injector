686fad37f24c9aabd3667e98a802ad2fd7380aa6
memzero <90333643+rkmllh@users.noreply.github.com>
2021-12-03 00:17:31 +0100

    Update macro.h

--- macro.h
+++ macro.h
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdio.h>
 
+typedef int pid_t;
+
 #define handle_error(what)		\
 	do{fprintf(stderr, "handle_error in %s <%d>\n", what, GetLastError()); exit(1);} while(0);
 
